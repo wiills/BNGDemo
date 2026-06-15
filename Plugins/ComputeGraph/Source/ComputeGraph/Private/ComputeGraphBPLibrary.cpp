b6730c5b252230569b4a0756f664640684c42146
wiills <me@wiills.com>
2026-06-15 18:29:26 +0800

    scope graph 计算图

---
+++ Plugins/ComputeGraph/Source/ComputeGraph/Private/ComputeGraphBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ComputeGraphBPLibrary.h"
+#include "ComputeGraph.h"
+
+UComputeGraphBPLibrary::UComputeGraphBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UComputeGraphBPLibrary::ComputeGraphSampleFunction(float Param)
+{
+	return -1;
+}
+
